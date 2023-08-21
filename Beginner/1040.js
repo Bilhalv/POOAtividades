7cea9f0675531f2eed3795318e938abf4141e468
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-08-21 09:18:43 -0300

    Update 1040.js

--- Beginner/1040.js
+++ Beginner/1040.js
@@ -1,19 +1,18 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 var lines = input.split("\n");
-let length = lines.length;
 let nums1 = lines.shift().split(" ");
 let [a, b, c, d] = nums1.map((x) => +x);
-let media1 = (a + b + c + d) / 4;
+let media1 = +((a*2 + b*3 + c*4 + d*1) / 10).toFixed(1);
 let e1 = lines.shift();
 let e = +e1;
 
-console.log("Media: " + media1.toFixed(1));
+console.log("Media: " + media1);
 if (media1 >= 7) {
   console.log("Aluno aprovado.");
-} else if (media1 >= 5.9b v) {
+} else if (media1 >= 5) {
   console.log("Aluno em exame.");
   console.log("Nota do exame: " + e.toFixed(1));
-  if ((media1 + e) / 2 >= 5) {
+  if (((media1 + e) / 2) >= 5) {
     console.log("Aluno aprovado.");
   } else {
     console.log("Aluno reprovado.");
@@ -22,4 +21,3 @@ if (media1 >= 7) {
 } else {
   console.log("Aluno reprovado.");
 }
-
