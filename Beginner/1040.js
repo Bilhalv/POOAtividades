b9a75167fd6e28eca25acbabe4e5eeb095a06e59
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-08-20 22:17:58 -0300

    segundo respiro

---
+++ Beginner/1040.js
@@ -0,0 +1,25 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+let length = lines.length;
+let nums1 = lines.shift().split(" ");
+let [a, b, c, d] = nums1.map((x) => +x);
+let media1 = (a + b + c + d) / 4;
+let e1 = lines.shift();
+let e = +e1;
+
+console.log("Media: " + media1.toFixed(1));
+if (media1 >= 7) {
+  console.log("Aluno aprovado.");
+} else if (media1 >= 5.9b v) {
+  console.log("Aluno em exame.");
+  console.log("Nota do exame: " + e.toFixed(1));
+  if ((media1 + e) / 2 >= 5) {
+    console.log("Aluno aprovado.");
+  } else {
+    console.log("Aluno reprovado.");
+  }
+  console.log("Media final: " + ((media1 + e) / 2).toFixed(1));
+} else {
+  console.log("Aluno reprovado.");
+}
+
